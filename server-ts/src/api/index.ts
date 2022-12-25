cb7863e8165b5739b923a93f92ee3fb1666b04d9
นฤเบศร โสพรรณรัตน์ <bosschikung@gmail.com>
2022-12-25 22:17:07 +0700

    สร้าง  userResult api

--- server-ts/src/api/index.ts
+++ server-ts/src/api/index.ts
@@ -1,6 +1,6 @@
 import Router from "koa-router"
 import annoucement from './announcement'
-
+import user_result from './user_result'
 const router = new Router()
 
 router
@@ -9,4 +9,6 @@ router
     })
 
     .use('/api/announcement', annoucement.routes())
+    .use('/api/user_result', user_result.routes())
+    
 export default router
