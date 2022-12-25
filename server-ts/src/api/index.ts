2d1d522a8eb7f8f6317a7b9d011c7616ff837e1c
นฤเบศร โสพรรณรัตน์ <bosschikung@gmail.com>
2022-12-25 21:39:46 +0700

    สร้าง announcement api

--- server-ts/src/api/index.ts
+++ server-ts/src/api/index.ts
@@ -1,8 +1,12 @@
 import Router from "koa-router"
+import annoucement from './announcement'
 
 const router = new Router()
-router.get('/api/greet', async (ctx, next) => {
-    ctx.body = {msg: 'Hello world.'}
-})
 
+router
+    .get('/api/greet', async (ctx, next) => {
+    ctx.body = {msg: 'Hello world.'}
+    })
+
+    .use('/api/announcement', annoucement.routes())
 export default router
--- server-ts/src/index.ts
+++ server-ts/src/index.ts
@@ -1,7 +1,9 @@
 import Koa from 'koa'
 import api from './api'
 
-const app = new Koa()
-app.use(api.routes())
 
-app.listen(8000)
+const app = new Koa()
+
+app
+    .use(api.routes())
+    .listen(8000)
