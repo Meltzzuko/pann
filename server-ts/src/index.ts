da2ddff7cc9fdf33079f158bb60836c10f9ada7b
นฤเบศร โสพรรณรัตน์ <bosschikung@gmail.com>
2022-12-31 15:49:48 +0700

    สร้าง utils

--- server-ts/src/index.ts
+++ server-ts/src/index.ts
@@ -1,5 +1,7 @@
 import Koa from 'koa'
 import api from './api'
+import loadFixtures from './fixtures'
+import appConfig from './config'
 
 
 const app = new Koa()
@@ -7,3 +9,5 @@ const app = new Koa()
 app
     .use(api.routes())
     .listen(8000)
+
+loadFixtures(appConfig.clearDataBeforeLodingFixture)
