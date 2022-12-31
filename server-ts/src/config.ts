82730e54f482b5775b61317784d938f129688b29
นฤเบศร โสพรรณรัตน์ <bosschikung@gmail.com>
2022-12-31 14:08:52 +0700

    import db conn

--- server-ts/src/config.ts
+++ server-ts/src/config.ts
@@ -1,8 +1,11 @@
+import dbConnectionInfo from './db-conn.json'
 const isProd = process.env.NODE_ENV == 'production'
 let appConfig = {
     isProd,
     isDev : !isProd,
     clearDataBeforeLodingFixture: isProd ? false : true,
+    dbConnectionInfo,
+    
 }
 
 export default appConfig
