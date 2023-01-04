0fd80605b49d55b6c7095857c46ce5717fc99aeb
นฤเบศร โสพรรณรัตน์ <bosschikung@gmail.com>
2023-01-04 17:40:02 +0700

    use koa-body/koa-cors and RestAPI

--- server-ts/src/index.ts
+++ server-ts/src/index.ts
@@ -1,4 +1,7 @@
 import Koa from 'koa'
+import { koaBody } from 'koa-body'
+import cors from '@koa/cors'
+
 import api from './api'
 import loadFixtures from './fixtures'
 import appConfig from './config'
@@ -7,6 +10,9 @@ import appConfig from './config'
 const app = new Koa()
 
 app
+    .use(koaBody())
+    .use(cors())
+    
     .use(api.routes())
     .listen(8000)
 
