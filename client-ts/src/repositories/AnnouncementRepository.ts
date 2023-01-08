5003fa3593436941ec83b98c35cfef827dc103fe
นฤเบศร โสพรรณรัตน์ <bosschikung@gmail.com>
2023-01-08 20:49:52 +0700

    สร้าง UserResult Repo

--- client-ts/src/repositories/AnnouncementRepository.ts
+++ client-ts/src/repositories/AnnouncementRepository.ts
@@ -1,17 +1,17 @@
 import Announcement from "../models/Announcement";
 
-export interface IRepository<T> {
-    getAll(filter: any|undefined): Promise<T[] | null>;
+export interface IRepository<T> { 
+  getAll(filter: any|undefined): Promise<T[] | null>; 
 }
 
-export class AnnouncementRepository implements IRepository<Announcement>{
-    
-    async getAll(): Promise<Announcement[] | null> {
+export class AnnouncementRepository implements IRepository<Announcement> {
+  
+  async getAll(): Promise<Announcement[] | null> {
 
-        return [
-            { id: 1, topic: '240-124 Midterm 1/2566', description: 'คะแนนกลางภาควิชา Web Dev', pubDateTime: '2022-09-08 10:00:00', userCode: 'suthon.s'},
-            { id: 2, topic: 'ทุนเรียนดีประจำปี 2566', description: 'test', remarkIfPositive: 'ขอแสดงความยินดีกับผุ้ได้รับทุนเรียนดีทุกคนด้วย', pubDateTime: '2022-09-09 15:00:00', userCode: 'suthon.s'}
-        ]
+    return [
+      { id: 1, topic: '240-124 Midterm 1/2566', description: 'คะแนนกลางภาควิชา Web Dev', pubDateTime: '2022-09-08 10:00:00', userCode: 'suthon.s'},
+      { id: 2, topic: 'ทุนเรียนดีประจำปี 2566', description: 'test', remarkIfPositive: 'ขอแสดงความยินดีกับผุ้ได้รับทุนเรียนดีทุกคนด้วย', pubDateTime: '2022-09-09 15:00:00', userCode: 'suthon.s'}
+    ]
 
-    }
+  }
 }
