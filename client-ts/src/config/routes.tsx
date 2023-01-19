55ac8ecf3ffd248da2f6fda13669faf7676504aa
นฤเบศร โสพรรณรัตน์ <bosschikung@gmail.com>
2023-01-19 14:34:13 +0700

    add AnnouncementListPage

--- client-ts/src/config/routes.tsx
+++ client-ts/src/config/routes.tsx
@@ -2,6 +2,7 @@ import { Navigate, Route, Routes, useLocation } from "react-router-dom"
 
 import Login from "../pages/login"
 import UserResultList from '../pages/user-result-list';
+import AnnouncementList from "../pages/announcement-list";
 import { useAppCtx } from "../AppProvider";
 
 type Props = {
@@ -30,6 +31,7 @@ const AppRoutes = () => {
             <Route index element={<Login />} />
             <Route path="login" element={<Login />}/>
             <Route path="home" element={<ProtectedRoute><UserResultList/></ProtectedRoute>} />
+            <Route path="announcement" element={<ProtectedRoute><AnnouncementList/></ProtectedRoute>} />
         </Routes>
     )
 }
